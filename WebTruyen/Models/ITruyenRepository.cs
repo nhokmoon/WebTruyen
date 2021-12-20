37a1b8518b7129c5d14337091a846cf3b26feff9
QuangDuy <lqd265@gmail.com>
2021-12-20 16:47:36 +0700

    add admin_page

--- WebTruyen/Models/ITruyenRepository.cs
+++ WebTruyen/Models/ITruyenRepository.cs
@@ -5,5 +5,8 @@ namespace WebTruyen.Models
     public interface ITruyenRepository
     {
         IQueryable<Truyen> Truyens { get; }
+        void SaveTruyen(Truyen truyen);
+
+        Truyen DeleteTruyen(int truyenId);
     }
 }
